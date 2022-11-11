77a403b081710cb86bb5ec2c981d697a0cbb5d8c
chinton1234 <98258863+chinton1234@users.noreply.github.com>
2022-11-11 20:01:06 +0700

    add check

--- go.mod
+++ go.mod
@@ -8,10 +8,21 @@ require (
 )
 
 require (
+	github.com/armon/go-metrics v0.3.10 // indirect
+	github.com/fatih/color v1.9.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
+	github.com/hashicorp/go-hclog v0.14.1 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.0 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/hashicorp/serf v0.9.7 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -27,19 +38,20 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/hashicorp/consul/api v1.15.3
 	github.com/joho/godotenv v1.4.0
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/rabbitmq/amqp091-go v1.5.0
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	go.mongodb.org/mongo-driver v1.10.3
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
+	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- main.go
+++ main.go
@@ -7,9 +7,84 @@ import (
 	controllers "matchingService/controller"
 
 	"github.com/gin-gonic/gin"
+
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	consulapi "github.com/hashicorp/consul/api"
 )
 
+func serviceRegistryWithConsul() {
+	config := consulapi.DefaultConfig()
+	consul, err := consulapi.NewClient(config)
+	if err != nil {
+		log.Println(err)
+	}
+
+	serviceID := "matching-service1"
+	port, _ := strconv.Atoi(getPort()[1:len(getPort())])
+	address := getHostname()
+
+	registration := &consulapi.AgentServiceRegistration{
+		ID:      serviceID,
+		Name:    "matching-service",
+		Port:    port,
+		Address: getHostname(),
+		Check: &consulapi.AgentServiceCheck{
+			HTTP:     fmt.Sprintf("http://%s:%v/check", address, port),
+			Interval: "10s",
+			Timeout:  "30s",
+		},
+	}
+
+	regiErr := consul.Agent().ServiceRegister(registration)
+
+	if regiErr != nil {
+		log.Printf("Failed to register service: %s:%v ", address, port)
+	} else {
+		log.Printf("successfully register service: %s:%v", address, port)
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+func check(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Consul check")
+}
+
+func getPort() (port string) {
+	port = os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	port = ":" + port
+	return
+}
+
+func getHostname() (hostname string) {
+	hostname, _ = os.Hostname()
+	return
+}
+
 func main() {
+	serviceRegistryWithConsul()
 
 	PORT := os.Getenv("PORT")
 
@@ -23,6 +98,11 @@ func main() {
 	r.GET("/matching/:matchingId", controllers.GetMatching())
 	r.GET("/getMatchingByActivity/:activityId", controllers.GetMatchingByActivity())
 	r.GET("/getActivitiesByParticipant/:userId", controllers.GetMatchingByParticaipant())
-	r.Run("localhost:" + PORT)
+	r.GET("/check", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"check": "ok",
+		})
 
+	})
+	r.Run("localhost:" + PORT)
 }
